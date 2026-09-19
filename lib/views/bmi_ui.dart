f010aef45c9fd2de092e9605bf2bfb2fd7eeed8a
godzila1605-collab <godzila1605@gmail.com>
2026-09-19 18:09:35 +0700

    add bmi,bmr

--- lib/views/bmi_ui.dart
+++ lib/views/bmi_ui.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sort_child_properties_last
+
 import 'package:flutter/material.dart';
 
 class BmiUI extends StatefulWidget {
@@ -11,7 +13,155 @@ class _BmiUIState extends State<BmiUI> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 50, 241, 11),
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(40.0),
+            child: Column(
+              children: [
+                Text(
+                  'คำนวนดัชนีมวลกาย (BMI)',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: const Color.fromARGB(255, 0, 0, 0),
+                    fontSize: MediaQuery.of(context).size.height * 0.028,
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Image.asset(
+                  'assets/images/bmi.png',
+                  width: MediaQuery.of(context).size.height * 0.15,
+                  height: MediaQuery.of(context).size.height * 0.15,
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(height: 10.0),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "น้ำหนัก (kg.)",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w100,
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                      borderSide: const BorderSide(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        width: 2.0,
+                      ),
+                    ),
+                    hintText: 'กรอกน้ำหนักของคุณ',
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+                    filled: true,
+                    fillColor: const Color.fromARGB(255, 253, 253, 253),
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "ส่วนสูง (cm.)",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w100,
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                      borderSide: const BorderSide(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        width: 2.0,
+                      ),
+                    ),
+                    hintText: 'กรอกส่วนสูงของคุณ',
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+                    filled: true,
+                    fillColor: const Color.fromARGB(255, 253, 253, 253),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(
+                      MediaQuery.of(context).size.width,
+                      MediaQuery.of(context).size.height * 0.055,
+                    ),
+                    backgroundColor: Color.fromARGB(255, 255, 65, 0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25)),
+                  ),
+                  child: Text('คำนวณ BMI',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white)),
+                ),
+                SizedBox(height: 15.0),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(
+                      MediaQuery.of(context).size.width,
+                      MediaQuery.of(context).size.height * 0.05,
+                    ),
+                    backgroundColor: Colors.grey,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25)),
+                  ),
+                  child: Text('ล้างข้อมูล',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white)),
+                ),
+                SizedBox(height: 20.0),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.symmetric(vertical: 35),
+                  color: Color.fromARGB(255, 200, 230, 201),
+                  child: Column(
+                    children: [
+                      Text('BMI',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      Text('0.00',
+                          style: TextStyle(
+                              fontSize: 32,
+                              color: Color.fromARGB(255, 255, 65, 0))),
+                      Text('การแปรผล',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/views/bmr_ui.dart
+++ lib/views/bmr_ui.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sort_child_properties_last
+
 import 'package:flutter/material.dart';
 
 class BmrUI extends StatefulWidget {
@@ -8,10 +10,270 @@ class BmrUI extends StatefulWidget {
 }
 
 class _BmrUIState extends State<BmrUI> {
+  bool isMale = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 238, 113, 255),
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(40.0),
+            child: Column(
+              children: [
+                Text(
+                  'คำนวนหาอัตราการเผาผลาญที่ร่างกายต้องการ (BMR)',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: const Color.fromARGB(255, 0, 0, 0),
+                    fontSize: MediaQuery.of(context).size.height * 0.028,
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Image.asset(
+                  'assets/images/bmr.png',
+                  width: MediaQuery.of(context).size.height * 0.15,
+                  height: MediaQuery.of(context).size.height * 0.15,
+                  fit: BoxFit.cover,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'เพศ',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w100,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5.0),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            isMale = true;
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(
+                              0, MediaQuery.of(context).size.height * 0.055),
+                          backgroundColor: isMale
+                              ? Color.fromARGB(255, 179, 217, 242)
+                              : Colors.white,
+                          elevation: isMale ? 0 : 1,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        child: Text('ชาย',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: isMale
+                                  ? FontWeight.normal
+                                  : FontWeight.normal,
+                              color: !isMale
+                                  ? Colors.black
+                                  : const Color.fromARGB(255, 0, 105, 255),
+                            )),
+                      ),
+                    ),
+                    SizedBox(width: 10.0),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            isMale = false;
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(
+                              0, MediaQuery.of(context).size.height * 0.055),
+                          backgroundColor: !isMale
+                              ? Color.fromARGB(255, 179, 217, 242)
+                              : Colors.white,
+                          elevation: !isMale ? 0 : 1,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        child: Text('หญิง',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: !isMale
+                                  ? FontWeight.normal
+                                  : FontWeight.normal,
+                              color: !isMale
+                                  ? const Color.fromARGB(255, 0, 105, 255)
+                                  : Colors.black,
+                            )),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10.0),
+                SizedBox(height: 5.0),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "น้ำหนัก (kg.)",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w100,
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5.0),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                      borderSide: const BorderSide(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        width: 2.0,
+                      ),
+                    ),
+                    hintText: 'กรอกน้ำหนักของคุณ',
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+                    filled: true,
+                    fillColor: const Color.fromARGB(255, 253, 253, 253),
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "ส่วนสูง (cm.)",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w100,
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5.0),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                      borderSide: const BorderSide(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        width: 2.0,
+                      ),
+                    ),
+                    hintText: 'กรอกส่วนสูงของคุณ',
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+                    filled: true,
+                    fillColor: const Color.fromARGB(255, 253, 253, 253),
+                  ),
+                ),
+                SizedBox(height: 5.0),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "อายุ (ปี)",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w100,
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5.0),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                      borderSide: const BorderSide(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        width: 2.0,
+                      ),
+                    ),
+                    hintText: 'กรอกอายุของคุณ',
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+                    filled: true,
+                    fillColor: const Color.fromARGB(255, 253, 253, 253),
+                  ),
+                ),
+                SizedBox(height: 15.0),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(
+                      MediaQuery.of(context).size.width,
+                      MediaQuery.of(context).size.height * 0.055,
+                    ),
+                    backgroundColor: Color.fromARGB(255, 255, 65, 0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25)),
+                  ),
+                  child: Text('คำนวณ BMR',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white)),
+                ),
+                SizedBox(height: 10.0),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(
+                      MediaQuery.of(context).size.width,
+                      MediaQuery.of(context).size.height * 0.05,
+                    ),
+                    backgroundColor: Colors.grey,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25)),
+                  ),
+                  child: Text('ล้างข้อมูล',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white)),
+                ),
+                SizedBox(height: 15.0),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  color: Color.fromARGB(255, 200, 230, 201),
+                  child: Column(
+                    children: [
+                      Text('BMI',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      Text('0.00',
+                          style: TextStyle(
+                              fontSize: 32,
+                              color: Color.fromARGB(255, 255, 65, 0))),
+                      Text('การแปรผล',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
